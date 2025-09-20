d5b38526adda14375e078b5ba1049df02ed34a90
suyashkanase <kanasesuyash01@gmail.com>
2025-09-20 20:13:42 +0530

    Add files via upload

---
+++ Automate-whatsapp-message-main/Automate-whatsapp-message-main/README.md
@@ -0,0 +1,2 @@
+# Automate-whatsapp-message
+A simple code that send message on whatsapp
---
+++ Automate-whatsapp-message-main/Automate-whatsapp-message-main/whatsapp.py
@@ -0,0 +1,4 @@
+import pywhatkit as pwk
+
+#number,message,hours,minuites
+pwk.sendwhatmsg('+91xxxxxxxxx5','Hello suyash',9,45)
